f89ce1a131d468b0310754f513d7edc44e13ba5a
Wesley <league4orce@gmail.com>
2026-01-29 01:36:13 +0800

    Rename Duke to Hermes and added Greet and Exit message

---
+++ src/main/java/Hermes.java
@@ -0,0 +1,11 @@
+public class Hermes {
+    public static void main(String[] args) {
+        String logo = "___________________________________\n"
+                    + "Hello! I'm Hermes!\n"
+                    + "What can I do for you?\n"
+                    + "___________________________________\n"
+                    + "Bye! Hope to see you again soon!\n"
+                    + "___________________________________\n";
+        System.out.println(logo);
+    }
+}
