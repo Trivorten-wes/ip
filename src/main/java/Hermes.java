02f0fd0b68963e185d5b301c5957be44ffc9563f
Wesley <league4orce@gmail.com>
2026-01-30 01:57:06 +0800

    Level 1 Basic echo function added to chatbot

--- src/main/java/Hermes.java
+++ src/main/java/Hermes.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
+
 public class Hermes {
     public static void main(String[] args) {
-        String logo = "___________________________________\n"
-                    + "Hello! I'm Hermes!\n"
-                    + "What can I do for you?\n"
-                    + "___________________________________\n"
-                    + "Bye! Hope to see you again soon!\n"
-                    + "___________________________________\n";
-        System.out.println(logo);
+        Scanner in = new Scanner(System.in);
+        String line = "        ___________________________________\n";
+        String greeting = "Hello! I'm Hermes!\n"
+                + "        What can I do for you?";
+        String bye = line + "        Bye! Hope to see you again soon!\n" + line;
+
+        String message = greeting;
+        do {
+            System.out.print(line);
+            System.out.println("        " + message);
+            System.out.print(line);
+            message = in.nextLine();
+        } while (!message.equals("bye"));
+
+        System.out.print(bye);
     }
 }
