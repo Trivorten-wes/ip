8aeccd4aaf526cf08c80271614da9bd31b62c565
Wesley <league4orce@gmail.com>
2026-01-30 02:18:58 +0800

    Level-2: Change inputs to add to a list that can be called

--- src/main/java/Hermes.java
+++ src/main/java/Hermes.java
@@ -3,18 +3,34 @@ import java.util.Scanner;
 public class Hermes {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String line = "        ___________________________________\n";
-        String greeting = "Hello! I'm Hermes!\n"
-                + "        What can I do for you?";
-        String bye = line + "        Bye! Hope to see you again soon!\n" + line;
+        String line = "       ___________________________________\n";
+        String indent = "        ";
+        String greeting = line + indent + "Hello! I'm Hermes the God of communication!\n"
+                + indent + "What can I do for you?\n" + line;
+        String bye = line + indent + "Bye! Hope to see you again soon!\n" + line;
 
-        String message = greeting;
-        do {
-            System.out.print(line);
-            System.out.println("        " + message);
-            System.out.print(line);
+        String[] list = new String[100];
+        int index = 0;
+
+        System.out.print(greeting);
+        String message = in.nextLine();
+
+        while (!message.equals("bye")) {
+            if (message.equals("list")) {
+                System.out.print(line);
+                for (int i = 0; i < index; i++) {
+                    System.out.println(indent + (i+1) + ". " + list[i]);
+                }
+                System.out.print(line);
+            } else {
+                System.out.print(line);
+                System.out.println("        added: " + message);
+                System.out.print(line);
+                list[index] = message;
+                index++;
+            }
             message = in.nextLine();
-        } while (!message.equals("bye"));
+        }
 
         System.out.print(bye);
     }
