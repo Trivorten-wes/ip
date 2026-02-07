f2ad3c84f5baa970debbc91fccdf46559abdd6d6
Wesley <league4orce@gmail.com>
2026-02-07 15:53:44 +0800

    A-TextUiTesting Edited inputs and expected outputs for testing

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -17,7 +17,7 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(from: " + start
+        return "[E]" + super.toString() + "(from: " + start
                 + " to: " + end +")";
     }
 
