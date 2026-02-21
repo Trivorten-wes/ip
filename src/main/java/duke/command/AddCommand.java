d6e46ada6259d537d6b0183a4e5612989686d974
Wesley <league4orce@gmail.com>
2026-02-22 00:02:59 +0800

    Implement Dates with add command

--- src/main/java/duke/command/AddCommand.java
+++ src/main/java/duke/command/AddCommand.java
@@ -3,11 +3,13 @@ package duke.command;
 import duke.TaskList;
 import duke.UI;
 import duke.exceptions.HermesMissingDescription;
-import duke.exceptions.HermesMissingTime;
-import duke.task.Deadline;
-import duke.task.Event;
-import duke.task.TaskType;
-import duke.task.Todo;
+import duke.exceptions.HermesInvalidTime;
+import duke.task.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class AddCommand implements Command{
     private final TaskType taskType;
@@ -16,21 +18,54 @@ public class AddCommand implements Command{
         this.taskType = taskType;
     }
 
-    @Override
-    public void execute(String description, TaskList tasks, UI ui) throws HermesMissingDescription, HermesMissingTime {
+    public Task silentExecute(String description, boolean isDone) throws HermesInvalidTime{
         switch (taskType) {
         case TODO:
-            tasks.add(new Todo(description));
-            ui.newTask(tasks.get(tasks.size() - 1), tasks.size() - 1);
-            break;
+            return new Todo(description);
         case DEADLINE:
-            tasks.add(new Deadline(description));
-            ui.newTask(tasks.get(tasks.size() - 1), tasks.size() - 1);
-            break;
+            String[] components = description.split("by:");
+            return new Deadline(components[0], parseDateTime(components[1]));
         case EVENT:
-            tasks.add(new Event(description));
-            ui.newTask(tasks.get(tasks.size() - 1), tasks.size() - 1);
-            break;
+            int fromIndex = description.indexOf("from:");
+            int toIndex = description.indexOf("to:");
+            LocalDateTime from;
+            LocalDateTime to;
+            String task;
+            if (fromIndex == -1 || toIndex == -1) {
+                throw new HermesInvalidTime();
+            } else if (fromIndex < toIndex) {
+                from = parseDateTime(description.substring(fromIndex + 5, toIndex));
+                to = parseDateTime(description.substring(toIndex + 3));
+                task = description.substring(0, fromIndex);
+            } else {
+                from = parseDateTime(description.substring(fromIndex + 5));
+                to = parseDateTime(description.substring(toIndex + 3, fromIndex));
+                task = description.substring(0, toIndex);
+            }
+            return new Event(task, from, to, isDone);
+        }
+        return null;
+    }
+
+    @Override
+    public void execute(String description, TaskList tasks, UI ui) throws HermesMissingDescription, HermesInvalidTime {
+        tasks.add(silentExecute(description, false));
+        ui.newTask(tasks.get(tasks.size() - 1), tasks.size() - 1);
+    }
+
+    private LocalDateTime parseDateTime(String textInput) throws HermesInvalidTime{
+        DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter dFormatter  = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        try {
+            return LocalDateTime.parse(textInput.strip(), dtFormatter);
+        } catch (DateTimeParseException ignored) { }
+
+        try {
+            LocalDate date = LocalDate.parse(textInput.strip(), dFormatter);
+            return date.atStartOfDay();
+        } catch (DateTimeParseException e) {
+            throw new HermesInvalidTime();
         }
     }
 }
