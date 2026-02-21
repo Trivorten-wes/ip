f188cbb2b6abeeb97470a7c5535c1a4a36cc7396
Wesley <league4orce@gmail.com>
2026-02-22 01:03:13 +0800

    Level-9 Add Find as a command to the command enum list and created a FindCommand class

---
+++ src/main/java/duke/command/FindCommand.java
@@ -0,0 +1,22 @@
+package duke.command;
+
+import duke.TaskList;
+import duke.UI;
+import duke.exceptions.HermesMissingDescription;
+import duke.exceptions.HermesMissingTime;
+
+public class FindCommand implements Command {
+    @Override
+    public void execute(String description, TaskList tasks, UI ui) throws HermesMissingDescription, HermesMissingTime {
+        String[] keywords = description.split("\\s+");
+        TaskList matchingTasks = new TaskList();
+        for (String keyword : keywords) {
+            for (int i = 0; i < tasks.size(); i++) {
+                if (tasks.get(i).getDescription().contains(keyword)) {
+                    matchingTasks.add(tasks.get(i));
+                }
+            }
+        }
+        ui.showFound(matchingTasks);
+    }
+}
